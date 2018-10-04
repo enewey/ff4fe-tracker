0ff64fcc97faafd884c19afba7382760ad66641b
enewey <narcodis@gmail.com>
2018-10-03 18:28:05 -0600

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-npm run-script build && aws s3 sync build/ s3://ff4fe-tracker.enewey.com
+npm run-script build && \
+    aws s3 sync build/ s3://ff4fe-tracker.enewey.com && \
+    aws cloudfront create-invalidation --distribution-id E2WZK668UD8B4A --paths "/*"
